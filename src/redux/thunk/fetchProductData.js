0a363d7fe2ace8658228d589ae9128dea7de1cc1
sajid1545 <clashking1545@gmail.com>
2023-06-01 00:36:17 +0600

    search functionality added

--- src/redux/thunk/fetchProductData.js
+++ src/redux/thunk/fetchProductData.js
@@ -2,9 +2,9 @@ import { loadProduct } from '../actions/productAction';
 
 const fetchProductData = () => {
 	return async (dispatch, getState) => {
-		// const search = getState().filter.search;
+		const search = getState().filter.search;
 
-		const res = await fetch('http://localhost:5000/products');
+		const res = await fetch(`http://localhost:5000/products?text=${search}`);
 		const data = await res.json();
 
 		if (data.data.length) {
